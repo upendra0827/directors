c8c05b2153f80fab974d70c0bc7066996a393fdc
Maradana Upendra <upendramaradana4@gmail.com>
2022-04-14 10:26:24 +0530

    added cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const {
   editDirector,
   deleteDirector,
 } = require("./Routes/directorRoutes");
-const { getAllMovies, addMovie, getMovieById, editMovie, deleteMovie, getAllMoviesOfADirector,getMoviesByCategory, getMoviesByCelebrity } = require("./Routes/movieRoutes");
+const { getAllMovies, addMovie, getMovieById, editMovie, deleteMovie, getAllMoviesOfADirector, getMoviesByCategory, getMoviesByCelebrity } = require("./Routes/movieRoutes");
 
 const app = express();
 app.use(bodyParser.json());
@@ -16,22 +16,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function (req, res, next) {
 
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  const corsWhiteList = ['http://localhost:3000/', "https://the-imdb-project.herokuapp.com/"]
 
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  if (corsWhiteList.indexOf(req.headers.origin) !== -1) {
+    res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  //res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 
-  // Pass to next layer of middleware
-  next();
-});
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  }
+    next();
+  });
 
 app.get("/", (req, res) => {
   res.send("API is ready");
@@ -43,12 +40,12 @@ app.put("/director/:id", editDirector);
 app.delete("/director/:id", deleteDirector);
 app.get("/movies", getAllMovies);
 app.post("/movie", addMovie);
-app.get('/movie/:id',getMovieById);
-app.put('/movie/:id',editMovie);
-app.delete('/movie/:id',deleteMovie);
-app.get('/movies/:id',getAllMoviesOfADirector);
-app.get('/movies/category/:category',getMoviesByCategory);
-app.get('/movies/celebrity/:celebrity',getMoviesByCelebrity);
+app.get('/movie/:id', getMovieById);
+app.put('/movie/:id', editMovie);
+app.delete('/movie/:id', deleteMovie);
+app.get('/movies/:id', getAllMoviesOfADirector);
+app.get('/movies/category/:category', getMoviesByCategory);
+app.get('/movies/celebrity/:celebrity', getMoviesByCelebrity);
 
 const PORT = process.env.PORT || 3300;
 app.listen(PORT, () => {
